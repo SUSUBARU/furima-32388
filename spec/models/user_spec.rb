97efc5fc5507aaaf39cba3c3a2f50367f83e2392
SUSUBARU <go06and07go03@gmail.com>
2021-01-10 12:16:21 +0900

    rspec編集2

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -80,7 +80,31 @@ describe User do
         @user.valid?
         expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
       end
-      
+      it "last_nameが存在しなければ登録できない" do
+        @user.last_name = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name can't be blank")
+      end
+      it "first_nameが存在しなければ登録できない" do
+        @user.first_name = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name can't be blank")
+      end
+      it "last_name_readingが存在しなければ登録できない" do
+        @user.last_name_reading = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name reading can't be blank")
+      end
+      it "first_name_readingが存在しなければ登録できない" do
+        @user.first_name_reading = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name reading can't be blank")
+      end
+      it "birthdayが存在しなければ登録できない" do
+        @user.birthday = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Birthday can't be blank")
+      end
     end
   end
 end
