51ebc39ba0a5739f9a9aa79cb4a0e5f9ecf5e580
SUSUBARU <go06and07go03@gmail.com>
2021-01-15 23:33:12 +0900

    itemモデル記述省略

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,16 +3,18 @@ class Item < ApplicationRecord
   has_one :user_item
   has_one_attached :image
 
-  validates :image, presence: true
-  validates :name, presence: true
-  validates :explain, presence: true
-  validates :category_id, presence: true
-  validates :state_id, presence: true
-  validates :burden_id, presence: true
-  validates :prefecture_id, presence: true
-  validates :delivery_date_id, presence: true
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :explain
+    validates :category_id
+    validates :state_id
+    validates :burden_id
+    validates :prefecture_id
+    validates :delivery_date_id
+  end
   with_options presence: true, format: { with: /\A[\d]+\z/, message: 'には半角数字を使用してください'  } do
-    validates :price, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range" }
+    validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range" }
   end
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
@@ -21,11 +23,13 @@ class Item < ApplicationRecord
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :delivery_date
 
-  validates :category_id, numericality: { other_than: 1 } 
-  validates :state_id, numericality: { other_than: 1 }
-  validates :burden_id, numericality: { other_than: 1 }
-  validates :prefecture_id, numericality: { other_than: 1 }
-  validates :delivery_date_id, numericality: { other_than: 1 }
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :state_id
+    validates :burden_id
+    validates :prefecture_id
+    validates :delivery_date_id
+  end
 
   def was_attached?
     self.image.attached?
