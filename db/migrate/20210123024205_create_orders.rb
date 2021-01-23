f8d87b177774571be0f01decbdcb0a0c68e75027
SUSUBARU <go06and07go03@gmail.com>
2021-01-23 11:47:25 +0900

    orderテーブル編集1

--- db/migrate/20210123024205_create_orders.rb
+++ db/migrate/20210123024205_create_orders.rb
@@ -1,7 +1,10 @@
 class CreateOrders < ActiveRecord::Migration[6.0]
   def change
     create_table :orders do |t|
-
+      t.integer :user_id
+      t.integer :item_id
+      t.references :user, null: false, foreign_key: true
+      t.references :item, null: false, foreign_key: true
       t.timestamps
     end
   end
