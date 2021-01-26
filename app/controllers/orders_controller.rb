f604c06c8e290cbfc7d8a9363933b296797e8643
SUSUBARU <go06and07go03@gmail.com>
2021-01-26 23:43:26 +0900

    URL直接入力防止編集

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,11 @@
 class OrdersController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!
   def index
     @order = UserItem.new
     @item = Item.find(params[:item_id])
+    if current_user.id == @item.user.id || @item.order.present?
+      redirect_to root_path
+    end
   end
 
   def create
