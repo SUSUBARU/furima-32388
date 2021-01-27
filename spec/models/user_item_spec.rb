4edc0a516d814a46e8136f4bfed25626113b670b
SUSUBARU <go06and07go03@gmail.com>
2021-01-27 21:57:46 +0900

    user_itemテストコード編集6

--- spec/models/user_item_spec.rb
+++ spec/models/user_item_spec.rb
@@ -72,6 +72,16 @@ RSpec.describe UserItem, type: :model do
         @user_item.valid?
         expect(@user_item.errors.full_messages).to include("Phone number can't be blank")
       end
+      it "電話番号が12桁以上であれば、登録ができない" do
+        @user_item.phone_number = "111111111111"
+        @user_item.valid?
+        expect(@user_item.errors.full_messages).to include("Phone number 半角数字11文字を入力してください")
+      end
+      it "電話番号についての情報が英数混合であれば、登録ができない" do
+        @user_item.phone_number = "aaa11111111"
+        @user_item.valid?
+        expect(@user_item.errors.full_messages).to include("Phone number 半角数字11文字を入力してください")
+      end
       it "電話番号についての情報が全角数字であれば、登録ができない" do
         @user_item.phone_number = "１１１１１１１１１１１"
         @user_item.valid?
